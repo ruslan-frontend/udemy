c16ebcaac1ed9528d691ebe9c570f73a921ca823
Ruslantarget <ruslan.bulatov.96@gmail.com>
2022-09-27 16:30:00 +0300

    lesson-29

--- js/script.js
+++ js/script.js
@@ -1,67 +1,71 @@
 "use strict"
 
-let numberOfFilms;
-
-function start() {
-  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 12);
-
-  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 12);
-  }
-}
-start();
-
 const personalMovieDB = {
-  count: numberOfFilms,
+  count: 0,
   movies: {},
   actors: {}, 
   genres: [],
-  privat: false
-};
-
-function rememberMyFilms() {
-  for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = +prompt('На сколько оцените его?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
-    personalMovieDB.movies[a] = b;
-      console.log('Done!');
-    } else {
-        console.log('Error!');
-      i--;
+  privat: false,
+  start: function() {
+    personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', 12);
+  
+    while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+      personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', 12);
     }
-  }
-}
-rememberMyFilms();
+  },
+  rememberMyFilms: function() {
+    for (let i = 0; i < 2; i++) {
+      const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = +prompt('На сколько оцените его?', '');
+  
+      if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+      personalMovieDB.movies[a] = b;
+        console.log('Done!');
+      } else {
+          console.log('Error!');
+        i--;
+      }
+    }
+  },
+  detectPersonalLevel: function() {
+    if (personalMovieDB.count < 10) {
+      console.log('Просмотрено довольно мало фильмов');
+    } else if (10 <= personalMovieDB.count < 30) {
+      console.log('Вы классический зритель');
+    } else if (personalMovieDB.count >= 30) {
+      console.log('Вы киноман');
+    } else {
+      console.log('Произошла ошибка');
+    }
+  },
+  showMyDB: function() {
+    if (personalMovieDB.privat == false) {
+      console.log(personalMovieDB);
+    } else {
+      console.log('Запрещено параметрами приватности');
+    }
+  },
+  toggleVisibleMyDB: function() {
+    if (personalMovieDB.privat) {
+      personalMovieDB.privat = false;
+    } else {
+      personalMovieDB.privat = true;
+    }
+  },
+  writeYourGenres: function() {
+    for (let i = 1; i <= 3; i++) {
+      let genre = prompt(`Ваш любимый жанр по номером ${i}`);
 
-function detectPersonalLevel() {
-  if (personalMovieDB.count < 10) {
-    console.log('Просмотрено довольно мало фильмов');
-  } else if (10 <= personalMovieDB.count < 30) {
-    console.log('Вы классический зритель');
-  } else if (personalMovieDB.count >= 30) {
-    console.log('Вы киноман');
-  } else {
-    console.log('Произошла ошибка');
-  }
-}
-detectPersonalLevel();
+      if (genre == '' || genre === null) {
+        console.log('Вы ввели некорректные данные или нажали отмена');
+        i--;
+      } else {
+        personalMovieDB.genres[i - 1] = genre; //! i-1 - Чтобы массив начинался с первой позиции
+      }
+    }
 
-function showMyDB() {
-  if (personalMovieDB.privat == false) {
-    console.log(personalMovieDB);
-  } else {
-    console.log('Запрещено параметрами приватности');
+    personalMovieDB.genres.forEach((item, i) => {
+      console.log(`Любимый жанр №${i + 1} - это ${item}`);
+    });
   }
-}
-showMyDB();
-
-function writeYourGenres() {
-  for (let i = 1; i <= 3; i++) {
-    const genres = prompt(`Ваш любимый жанр по номером ${i}`);
-    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр по номером ${i}`); //! i-1 - Чтобы массив начинался с первой позиции
-  }
-}
-writeYourGenres();
-// console.log(personalMovieDB);
+};
--- js/udemy.js
+++ js/udemy.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 //! 13 - Условия
 
 /* 
@@ -22,7 +22,6 @@ if (num < 49) {
 //? Тернарный оператор
 // (num === 50) ? console.log('Ok!') : console.log('Error!');
 
-
 //? Switch (строгое сравнение)
 
 /* 
@@ -43,7 +42,7 @@ switch (num) {
      */
 
 //! 14 - Циклы
-    // let num = 50;
+// let num = 50;
 
 /* 
     while (num <= 55) {
@@ -84,7 +83,6 @@ function showFirstMessage(text) {
 showFirstMessage("Hello World");
 console.log(num);
  */
-
 
 /* 
 function calc(a, b) {
@@ -158,6 +156,456 @@ console.log(parseInt(test)); // 12 - Округление до целых чис
 console.log(parseFloat(test)); // 12.2 - Округление до числа
  */
 
-//! 
-//! 
-//! 
+//! 19 - Callback функция
+
+/* 
+function first() {
+  // Do something
+  setTimeout(function() {
+    console.log(1);
+  }, 500);
+}
+
+function second() {
+  console.log(2);
+}
+
+first();
+second();
+*/
+
+/* 
+function learnJS(lang, callback) {
+  console.log(`I'm learning ${lang}`);
+  callback();
+}
+
+function done() {
+  console.log(`I'm Done!`);
+}
+
+learnJS('JavaScript', done);
+ */
+
+//! 20 - Объекты и деструктуризация
+/* 
+const options = {
+  name: 'test',
+  width: 1024,
+  height: 1024,
+  colors: {
+    border: 'black',
+    bg: 'red'
+  },
+  makeTest: function() {
+    console.log('Test');
+  }
+};
+
+options.makeTest();
+
+const {border, bg} = options.colors;
+console.log(border);
+ */
+
+// console.log(Object.keys(options).length); // Длинна массива
+
+/* console.log(options.name);
+
+delete options.name; //Удаление ключа
+
+console.log(options); */
+
+/* 
+for (let key in options) {
+  if (typeof(options[key]) === 'object') {
+    for (let i in options[key]) {
+      console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+    }
+  } else {
+    console.log(`Свойство ${key} имеет значение ${options[key]}`);
+  }
+}
+ */
+
+/* 
+let counter = 0
+for (let key in options) {
+  if (typeof(options[key]) === 'object') {
+    for (let i in options[key]) {
+      console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+      counter++;
+    }
+  } else {
+    console.log(`Свойство ${key} имеет значение ${options[key]}`);
+    counter++;
+  }
+}
+console.log(counter);
+ */
+
+//! 21 - Массивы и псевдомассивы
+/* 
+const arr = [11, 22, 3, 49, 16, 84];
+arr.sort(compareNum); // Сортировка массива чисел по порядку
+function compareNum(a, b) {
+  return a - b;
+}
+console.log(arr);
+ */
+/* 
+arr.pop(); //Удаляет последний элемент массива
+arr.push(10); // Добавляет элемент в конец массива
+
+console.log('______________________');
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+console.log('______________________');
+
+for (let value of arr) {
+  console.log(value);
+}
+console.log('______________________');
+
+arr.forEach(function (item, i, arr) {
+  console.log(`${i}: ${item} внутри массива ${arr}`);
+});
+console.log('______________________');
+ */
+
+/* 
+const str = prompt('', '');
+const products = str.split(', '); // Разделить на массив
+products.sort(); // Сортировка массива по алфавиту (только для строк)
+ */
+
+/* console.log(products.join(';')); */ // Перевести массив в строчку
+
+//! 22 - Передача по ссылке или по значению, Spread оператор
+/* 
+let a = 5,
+    b = a;
+
+b = b + 5;
+console.log(a);
+console.log(b);
+ */
+
+/* 
+const obj = {
+    a: 5,
+    b: 1
+};
+
+const copy = obj;
+
+copy.a = 10;
+
+console.log(copy);
+console.log(obj);
+ */
+
+//? Поверхностная копия объекта
+/* function copy(mainObj) {
+  let objCopy = {};
+
+  let key;
+  for (key in mainObj) {
+    objCopy[key] = mainObj[key];
+  }
+  return objCopy;
+}
+
+const numbers = {
+  a: 2,
+  b: 5,
+  c: {
+    x: 7,
+    y: 4
+  }
+};
+
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10; // Заменит в одном объекте
+newNumbers.c.x = 10; // Заменит в обоих объектах
+
+console.log(numbers);
+console.log(newNumbers);
+
+const add = {
+  d: 17,
+  e: 20
+};
+
+const clone = (Object.assign({}, add));
+clone.d = 20;
+console.log(add);
+console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice(); // Создание копии массива
+
+newArray[1] = 'asd';
+
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'faceboook'];
+
+      console.log(internet);
+
+function log(a, b, c) {
+  console.log(a);
+  console.log(b);
+  console.log(c);
+}
+
+const num = [2, 4, 7];
+log(...num);
+
+const array = ['a', 'b'];
+
+const newAarray = [...array];
+const q = {
+  one: 1,
+  two: 2
+};
+
+const newObj = {...q};
+console.log(newObj);
+ */
+
+//! 23 - Основы ООП
+/* 
+let str = 'some';
+let strObj = new String(str);
+
+console.log(typeof(str));
+console.log(typeof(strObj));
+
+console.dir([1,2,3]);
+
+const soldier = {
+  health: 400,
+  armor: 100,
+  sayHello: function() {
+    console.log('Hello!');
+  }
+};
+
+const jonh = Object.create(soldier);
+
+// const jonh = {
+//   health: 100
+// };
+
+// jonh.__proto__ = soldier;
+
+// Object.setPrototypeOf(jonh, soldier);
+
+console.log(jonh.armor);
+jonh.sayHello();
+ */
+
+//! 25 - консоль разработчика, breakpoints / sources
+//! 26 - динамическая типизация в JS
+
+//? To string
+/* 
+console.log(typeof(String(null)));
+console.log(typeof(String(12)));
+
+console.log(typeof(5 + '')); // Конкатенация
+
+const num = 5;
+console.log('https://vk.com/catalog/' + num);
+
+const fontSize = 26 + 'px';
+
+//? To Number
+
+console.log(typeof(Number('4')));
+console.log(typeof(+'4'));
+
+console.log(typeof(parseInt('15px', 10)));
+ */
+//? To Boolean
+
+/* 
+0, '', null, undefined, NaN; // False
+ */
+/* 
+let switcher = null;
+if (switcher) {
+  console.log('Working..');
+}
+
+switcher = 1;
+if (switcher) {
+  console.log('Working..');
+}
+
+
+console.log(typeof(Boolean('4'))); // Boolean
+
+
+console.log(typeof(!!'4')); // Boolean
+ */
+
+//! 27 - Задачи с собеседований
+
+//? Какое будет выведено значение: let x = 5; alert( x++ ); ?
+/* 
+let x = 5;
+console.log(x++); // 5
+ */
+
+//? Чему равно такое выражение: [ ] + false - null + true ?
+/* 
+console.log([] + false); // false
+console.log([] + false - null); // NaN
+ */
+
+//? Что выведет этот код: let y = 1; let x = y = 2; alert(x); ?
+/* 
+let y = 1;
+let x = y = 2;
+console.log(x); // 2
+ */
+
+//? Чему равна сумма [ ] + 1 + 2?
+/* 
+console.log([] + 1 + 2); // 12
+ */
+
+//? Что выведет этот код: alert( "1"[0] )?
+/* 
+alert( "1"[0]); // 1
+ */
+
+//? Чему равно 2 && 1 && null && 0 && undefined ?
+/* 
+console.log(2 && 1 && null && 0 && undefined); // null
+ */
+
+//? Есть ли разница между выражениями? !!( a && b ) и (a && b)?
+/* 
+console.log(!!( 1 && 2 ) === (1 && 2)); // boolean !== 2
+
+ */
+
+//? Что выведет этот код: alert( null || 2 && 3 || 4 ); ?
+/*               3    3    3
+console.log(null || 2 && 3 || 4);
+И запинается на лжи &&
+ИЛИ запинается на правде ||
+ */
+
+//? a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b ?
+/* 
+let a,
+    b;
+a = [1, 2, 3];
+b = [1, 2, 3];
+console.log(a == b);
+ */
+
+//? Что выведет этот код: alert( +"Infinity" ); ?
+/* 
+console.log(+"Infinity"); // Infinity (только в этом случае, число)
+ */
+
+//? Верно ли сравнение: "Ёжик" > "яблоко"?
+/* 
+console.log("Ёжик" > "яблоко"); // false
+Нужно смотреть ЮНИКОД, порядок символов
+ */
+
+//? Чему равно 0 || "" || 2 || undefined || true || falsе ?
+/* 
+console.log(0 || "" || 2 || undefined || true || falsе); // 2
+0 = false
+ */
+
+//! 28 - Получение элементов со страницы
+/* 
+const box = document.getElementById('box'); // Получение по ID
+const btns = document.getElementsByTagName('button'); // Получение по ТЭГУ
+const circles = document.getElementsByClassName('circle'); // Получение по КЛАССУ
+
+const hearts = document.querySelectorAll('.heart');
+hearts.forEach(item => {
+  console.log(item);
+});
+
+const oneHeart = document.querySelector('.heart'); // Первый элемент на странице
+ */
+
+//! 29 - Действия с элементами на странице
+/* 
+const box = document.getElementById("box"),
+      btns = document.getElementsByTagName("button"), 
+      circles = document.getElementsByClassName("circle"),
+      wrapper = document.querySelector('.wrapper'),
+      hearts = wrapper.querySelectorAll(".heart"),
+      oneHeart = wrapper.querySelector(".heart");
+
+box.style.backgroundColor = 'blue';
+box.style.width = '500px';
+box.style.cssText = 'background-color: orange; width: 500px'; // Через CSS
+
+btns[1].style.borderRadius = '100%';
+
+circles[2].style.backgroundColor = 'yellow';
+ */
+/* 
+for(let i = 0; i < hearts.length; i++) {
+  hearts[i].style.backgroundColor = 'blue';
+}
+ */
+/* 
+hearts.forEach(item => {
+  item.style.backgroundColor = 'blue';
+});
+ */
+
+// const div = document.createElement('div');
+// const text = document.createTextNode('Hello World');
+
+// div.classList.add('black');
+/* 
+wrapper.prepend(div); // До
+hearts[0].before(div);
+hearts[0].after(div);
+circles[0].remove(); // Удаление
+hearts[0].replaceWith(circles[0]); // Замена
+wrapper.append(div); // После
+*/
+
+//? Устаревшие конструкции
+/* 
+wrapper.appendChild(div);
+wrapper.insertBefore(div, hearts[2]);
+wrapper.removeChild(hearts[1]);
+wrapper.replaceChild(circles[0], hearts);
+ */
+
+//? Работа с текстом
+/* 
+div.textContent = 'Hello';
+div.innerHTML = '<h1>Hello World</h1>';
+div.insertAdjacentHTML('beforebegin', '<h2>Hi</h2>'); // Перед элементом
+div.insertAdjacentHTML('afterbegin', '<h2>Hi</h2>'); // После элемента
+div.insertAdjacentHTML('beforeend', '<h2>Hi</h2>'); 
+div.insertAdjacentHTML('afterend', '<h2>Hi</h2>'); 
+*/
+
+
+
+
+//! 26 - динамическая типизация в JS
